312dacdc5aac384dba86ae6ae4d275156f643a22
Po-Gr <grigorovichpk@mail.ru>
2026-06-14 21:15:35 +0300

    правки

--- src/test/java/wb/OnlineShoppingTest.java
+++ src/test/java/wb/OnlineShoppingTest.java
@@ -3,13 +3,11 @@ package wb;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 
 public class OnlineShoppingTest {
 
